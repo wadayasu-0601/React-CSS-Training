773540fe7d94b7aad8decee0c8d2991ac865a562
wadayasu-0601 <64833067+wadayasu-0601@users.noreply.github.com>
2022-08-18 00:10:37 +0900

    0818learning

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { CssModules } from "./components/CssModules";
+import { Emotion } from "./components/Emotion";
 import { InlineStyle } from "./components/InlineStyle";
+import { StyledJsx } from "./components/Styled";
+import { StyledComponents } from "./components/StyledComponents";
 import "./styles.css";
 
 export default function App() {
@@ -7,6 +10,9 @@ export default function App() {
     <div className="App">
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
+      <StyledComponents />
+      <Emotion />
     </div>
   );
 }
---
+++ src/components/StyledComponents.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+export const StyledComponents = () => {
+  return (
+    <SContainer>
+      <STytle>StyledComponents</STytle>
+      <Sbutton>Fight</Sbutton>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  border: solid 2px #392eff;
+  border-radius: 20px;
+  padding: 8px;
+  margin: 8px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const STytle = styled.p`
+  margin: 0;
+  color: #3d84a8;
+`;
+
+const Sbutton = styled.button`
+  background-color: #abedd8;
+  border: none;
+  padding: 8px;
+  border-radius: 8px;
+  &:hover {
+    background-color: #46cdcf;
+    color: #fff;
+    cursor: pointer;
+  }
+`;
